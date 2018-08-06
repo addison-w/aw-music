878b6154aa3b0edfcd28dc1dba94964ed88f79e5
Addison Wen <addisonwen@Addisons-MacBook-Pro.local>
2018-08-06 16:06:55 +1000

    progress-bar

---
+++ src/base/progress-bar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bar-container">
+        <div class="inner-bar">
+            <div class="progress"></div>
+            <div class="btn-wrapper">
+                <div class="contrl-btn"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
