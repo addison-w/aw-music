3a5e338c584125074a207c76c3f65f9a772abb4b
Addison Wen <perhaps0660@gmail.com>
2018-09-01 10:31:39 +1000

    api url changed

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 export const SUCC_CODE = 200
-export const BASE_URL = 'http://207.148.85.183:3000'
+export const BASE_URL = 'http://api.momentums.us'
 // export const PROXY = 'http://158.69.209.100:80'
 export const PROXY = 'http://223.202.204.195:80'
 export const CDN_URL = '113.107.57.64'
